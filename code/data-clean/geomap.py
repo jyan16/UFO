e4058f0bc1c800ffcd81d7a67f9e4967139223a2
jyan16 <jin_yan@brown.edu>
2017-03-12 11:28:20 -0400

    location_info done

--- code/data-clean/geomap.py
+++ code/data-clean/geomap.py
@@ -11,30 +11,48 @@ field = 'https://maps.googleapis.com/maps/api/geocode/json?'
 key = 'AIzaSyD_VpEeAmvHVFRb94Pz1LF7l_SoLHepnow'
 
 #change the value of i
-i=5
 
-event_file = open('../../data/location_'+str(i)+'.csv', 'r', encoding='utf-8')
-event_reader = csv.reader(event_file)
+def get_lng_lat():
+    i=5
 
-event_writer = csv.writer(open('../../data/location_'+str(i)+'_info.csv', 'w', encoding='utf-8'))
+    event_file = open('../../data/location_'+str(i)+'.csv', 'r', encoding='utf-8')
+    event_reader = csv.reader(event_file)
 
-geolist = []
+    event_writer = csv.writer(open('../../data/location_'+str(i)+'_info.csv', 'w', encoding='utf-8'))
 
-index = 1
-for row in event_reader:
-    print(index)
-    index += 1
-    try:
-        j = requests.get(url=field+'address='+str(row[1])+'&key='+key).json()
-        lat = j['results'][0]['geometry']['location']['lat']
-        lng = j['results'][0]['geometry']['location']['lng']
-        tmp = row
-        tmp.append(lat)
-        tmp.append(lng)
+    geolist = []
 
-        geolist.append(tmp)
-    except:
-        print('error')
+    index = 1
+    for row in event_reader:
+        print(index)
+        index += 1
+        try:
+            j = requests.get(url=field+'address='+str(row[1])+'&key='+key).json()
+            lat = j['results'][0]['geometry']['location']['lat']
+            lng = j['results'][0]['geometry']['location']['lng']
+            tmp = row
+            tmp.append(lat)
+            tmp.append(lng)
 
-for item in geolist:
-    event_writer.writerow(item)
+            geolist.append(tmp)
+        except:
+            print('error')
+
+    for item in geolist:
+        event_writer.writerow(item)
+
+def combine():
+    info_writer = csv.writer(open('../../data/location_info.csv', 'w', encoding='utf-8'))
+    info_writer.writerow(['location_id', 'city', 'state', 'lat', 'lng'])
+    for i in range(1,11):
+        info_reader = csv.reader(open('../../data/location_'+str(i)+'_info.csv', 'r', encoding='utf-8'))
+        for row in info_reader:
+            try:
+                info_writer.writerow([row[0], row[1].split(',')[0], row[1].split(',')[1], row[2], row[3]])
+            except:
+                print('error')
+
+
+if __name__ == '__main__':
+    #get_lng_lat()
+    combine()
