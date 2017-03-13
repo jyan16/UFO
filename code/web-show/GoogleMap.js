9ae058952f224db6efe79f576a728320c88d1510
Kitty317 <tianleibetty@163.com>
2017-03-13 10:30:03 -0400

    Update GoogleMap.js

--- code/web-show/GoogleMap.js
+++ code/web-show/GoogleMap.js
@@ -1,11 +1,22 @@
+var map, heatmap;
+
 function initMap() {
     var myLatLng = {lat: 40.78, lng: -97.21};
 
     // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
             center: myLatLng,
             scrollwheel: false,
-            zoom: 3
-        });
+            zoom: 4
+    });
+    heatmap = new google.maps.visualization.HeatmapLayer({
+    		data: getPoints(),
+    		map: map
+  	});
 
+  	function getPoints() {
+  		return [
+  			new google.maps.LatLng(37.782551, -122.445368)
+  		]
+  	};
 }
