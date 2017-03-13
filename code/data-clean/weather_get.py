08b70ffd37460edf2d67ff402f648dc0e59be46e
jyan16 <jin_yan@brown.edu>
2017-03-13 12:12:50 -0400

    database

--- code/data-clean/weather_get.py
+++ code/data-clean/weather_get.py
@@ -4,12 +4,15 @@ import json
 
 field1 = 'https://api.darksky.net/forecast/'
 field2 = '?exclude=hourly,flags,isd-stations,daily'
-key = ''
+key = '9d6e0098a074c6f8f077476aeb54b2f8'
 
 
 geo_reader = csv.reader(open('../../data/location_info.csv', 'r', encoding='utf-8'))
 ufo_reader = csv.reader(open('../../data/file_ufo_clean.csv', 'r', encoding='utf-8'))
-ufo_writer = csv.writer(open('../../data/file_ufo_weather.csv', 'w', encoding='utf-8'))
+ufo_file = open('../../data/file_ufo_lat.csv', 'w', encoding='utf-8')
+ufo_writer = csv.writer(ufo_file)
+weather_file = open('../../data/file_ufo_weather.csv', 'w', encoding='utf-8')
+weather_writer = csv.writer(weather_file)
 
 #read location information
 next(geo_reader, None)
@@ -19,39 +22,45 @@ for geo in geo_reader:
     geo_id_list.append(geo[0])
     geo_list.append(geo)
 
-#read ufo information
 
-ufo_writer.writerow(['event_id'.encode('utf-8'), 'time'.encode('utf-8'), 'city'.encode('utf-8'), 'state'.encode('utf-8'), 'shape'.encode('utf-8'),
-                             'duration'.encode('utf-8'), 'summary'.encode('utf-8'), 'lat'.encode('utf-8'), 'lng'.encode('utf-8'), 'weather'.encode('utf-8')])
+ufo_writer.writerow(['event_id', 'year', 'month', 'day', 'time', 'city', 'state', 'shape', 'duration', 'summary', 'lat', 'lng'])
 next(ufo_reader, None)
+
+weather_writer.writerow(['event_id', 'unix_time', 'summary', 'icon', 'precipIntensity', 'precipProbability', 'temperature', 'apparentTemperature',
+                        'dewPoint', 'humidity', 'windSpeed', 'windBearing', 'visibility', 'cloudCover', 'pressure', 'ozone'])
 ufo_list = []
+
+index = 1
 for ufo in ufo_reader:
+    print(index)
+    index += 1
+    if index%500==0:
+
+        #using close and open to write in the file
+        ufo_file.close()
+        ufo_file = open('../../data/file_ufo_lat.csv', 'a+',encoding='utf-8')
+        ufo_writer = csv.writer(ufo_file)
+
+        weather_file.close()
+        weather_file = open('../../data/file_ufo_weather.csv', 'a+',encoding='utf-8')
+        weather_writer = csv.writer(weather_file)
+
+
     if ufo[7] in geo_id_list and 'T' in ufo[1]:
         geo = geo_list[geo_id_list.index(ufo[7])][3:5]
-        query = geo[0]+','+geo[1]+','+ufo[1][2:-1]
+        date = ufo[1][2:-1].split('T')[0].split('-')
 
+        hour = ufo[1][2:-1].split('T')[1]
+        query = geo[0]+','+geo[1]+','+ufo[1][2:-1]
+        ufo = [ufo[0]]+date+[hour]+ufo[2:-1]+geo
+        ufo_writer.writerow(ufo)
         '''
         try:
             weather = requests.get(url=field1+key+'/'+query+field2).json()
             weather = weather['currently']
-            ufo.append(weather)
+            tmp = [ufo[0]]
+            tmp.append(weather)
+            weather_writer.writerow(tmp)
 
         except:
             print('error')'''
-
-        ufo_writer.writerow(ufo[0:-1]+geo)
-
-
-
-
-
-
-
-
-'''
-j = requests.get(url=field1+key+'/33.4255104,-111.9400054,1980-10-7T13:00:00'+field2)
-if j:
-    print(j.content.current)
-    print(j)
-
-print(j)'''
