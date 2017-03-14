94f0e035cefeb603c4cbdb96c967195b1b02d2c1
田蕾 <Kitty@tianleideMacBook-Air.local>
2017-03-13 22:29:29 -0400

    ...

--- code/web-show/GoogleMap.js
+++ code/web-show/GoogleMap.js
@@ -1,28 +1,43 @@
-var map, heatmap;
-
 function initMap() {
     var myLatLng = {lat: 40.78, lng: -97.21};
-
+    
     // Create a map object and specify the DOM element for display.
-    map = new google.maps.Map(document.getElementById('map'), {
-            center: myLatLng,
-            scrollwheel: false,
-            zoom: 4
-    });
-    heatmap = new google.maps.visualization.HeatmapLayer({
-    		data: getPoints(),
-    		map: map
-  	});
-
-    var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: myLatLng,
+      scrollwheel: false,
+      zoom: 4
     });
 
-  	function getPoints() {
-  		return [
-  			new google.maps.LatLng(37.782551, -122.445368)
-  		]
-  	};
+    d3.csv("file_ufo_lat.csv", function(data) {
+      //console.log(data);
+
+      var LatLng = [];
+      for (var i = 0; i < data.length; i ++) {
+        if (data[i].year == "2016") {
+          var item = [data[i].lat, data[i].lng];
+          console.log(item);  
+          LatLng.push(item); 
+        }
+      };
+
+      //console.log(LatLng);
+      res = [];   
+      for (var i = 0; i < LatLng.length; i++) {
+        var lat = LatLng[i][0];
+        var lng = LatLng[i][1];
+        var item = new google.maps.LatLng(lat, lng);
+        //console.log(LatLng[i][0]);
+        //console.log(LatLng[i][1]);
+        //console.log(item);
+        res.push(item);
+      }
+      //console.log(res);
+
+      var heatmap = new google.maps.visualization.HeatmapLayer({
+        data: res,
+        map: map
+      });
+
+    });
+
 }
