fedb24f3d8f4db362a0698cf4f1ff5eee3a59f6d
jyan16 <jin_yan@brown.edu>
2017-03-22 21:08:32 -0400

    web

--- code/data-clean/data_process.py
+++ code/data-clean/data_process.py
@@ -1,6 +1,6 @@
 import sqlite3
 import json
-
+import re
 
 
 
@@ -9,7 +9,6 @@ c = conn.cursor()
 
 def count_by_year():
     year_count_result = {}
-    max_count = 0
     for row in c.execute('''SELECT year, state, count(*)
                             FROM events
                             WHERE year>=1950 AND year<=2016
@@ -18,19 +17,21 @@ def count_by_year():
             year_count_result[row[0]] = [[row[1], row[2]]]
         else:
             year_count_result[row[0]].append([row[1], row[2]])
-        max_count = max(row[2], max_count)
-    print(max_count)
     result = []
     for key, value in year_count_result.items():
-        result.append({"year" : key, "state" : value})
+
+        total = sum([item[1] for item in value])
+        value_new = [[item[0], item[1]/total] for item in value]
+        result.append({"year": key, "frequency": value_new})
+
     result.sort(key = lambda x : x["year"])
-    with open("../../data/count_by_year.json", 'w') as outfile:
+    with open("../../data/frequency_by_year.json", 'w') as outfile:
         json.dump(result, outfile, indent = 4)
 
 def count_by_hour():
     hour_count_result = {}
     for row in c.execute('''SELECT time FROM events
-                            WHERE year>=1950'''):
+                            WHERE year>=1950 AND year<=2016'''):
         hour = row[0].split(':')[0]
         if hour not in hour_count_result.keys():
             hour_count_result[hour] = 1
@@ -42,9 +43,62 @@ def count_by_hour():
     with open("../../data/count_by_hour.json", 'w') as outfile:
         json.dump(result, outfile, indent = 4)
 
+def state_path_change():
 
+    pattern = re.compile(r'[A-Z]')
+    with open('../../data/statesPath.json', 'r') as f:
+        states =json.load(f)
+
+    state_path_new = []
+    for state in states:
+
+        tmp_points = re.split(pattern, state['d'])
+        tmp_point_list = []
+        tmp_chars = pattern.findall(state['d'])
+
+        for i in range(1,len(tmp_points)-1):
+            point = tmp_points[i]
+            if point != '':
+                tmp = point.split(',')
+                tmp[0] = str(float(tmp[0]) * 0.7)[0:9]
+                tmp[1] = str(float(tmp[1]) * 0.7)[0:9]
+                tmp_point_list.append(','.join(tmp))
+            else:
+                tmp_point_list.append(point)
+
+        path_changed = ''
+        for character, point in zip(tmp_chars, tmp_point_list):
+            path_changed += (character + point)
+
+        path_changed += tmp_chars[-1]
+        state['d'] = path_changed
+        state_path_new.append(state)
+
+    with open("../../data/statesPath_new.json", 'w') as outfile:
+        json.dump(state_path_new, outfile, indent = 4)
+
+def shape_count():
+
+    shapes = {}
+
+    for row in c.execute('''SELECT shape, count(*)
+                            FROM events
+                            WHERE year>=1950 AND year<=2016
+                            GROUP BY shape'''):
+        if row[0].lower() not in shapes.keys():
+            shapes[row[0].lower()] = row[1]
+        else:
+            shapes[row[0]] += row[1]
+
+    result = []
+    for key, value in shapes.items():
+        result.append({"shape" : key, "count" : value})
+
+    result.sort(key = lambda x : x["count"])
+    with open("../../data/shape_all.json", 'w') as outfile:
+        json.dump(result, outfile, indent = 4)
 if __name__ == '__main__':
-    count_by_year()
+    shape_count()
 
 
 
---
+++ docs/js/pie_shape.js
@@ -0,0 +1,38 @@
+var margin1 = {top: 19.5, right: 50, bottom: 19.5, left: 39.5};
+var width1 = 700 - margin1.right;
+var height1 = 400 - margin1.top - margin1.bottom;
+
+
+var svg2 = d3.select("#chart_pie_shape").append("svg")
+    .attr("width", width1 + margin1.left + margin1.right)
+    .attr("height", height1 + margin1.top + margin1.bottom)
+    .append("g")
+    .attr("class", "pie_shape")
+    .attr("transform", "translate(" + (margin1.left + width1 / 2) + "," + (margin1.top + height1 / 2) + ")")
+    
+
+d3.json("/data/shape_all.json", function(shapes) {
+
+    var pie = d3.pie()
+                .sort(null)
+                .value(function(d) {
+                    return d.count;
+                });
+    var arcs = svg2.selectAll(".arc")
+    			   .data(pie(shapes)).enter()
+    			   .append("g")
+    			   .attr("class", "arc")
+
+    var myarc = d3.arc()
+    			.outerRadius(200)
+    			.innerRadius(0); 
+    var colorScale = d3.scaleOrdinal([0, 1, 2, 3, 4, 5, 6])
+
+    var mycolor = ["1f77b4", "ff7f0e", "2ca02c", "d62728", "9467bd", "8c564b"]  
+
+    arcs.append("path")
+    	.attr("d", myarc)
+    	.style("fill", function(d) {
+    		console.log(colorScale(d.shape))
+    		return mycolor(colorScale(d.shape));})
+})
